84c36433219b80677da96844ca8c8b2d358598f0
lfire <yunzhi.li.08@gmail.com>
2017-02-12 08:49:20 +0800

    Ìí¼ÓÏîĿµ¥ÎļþÅäÖÃĿ¼

---
+++ src/common/config/aipro_liyz.js
@@ -0,0 +1,46 @@
+'use strict';
+/**
+ * the config of the asiainfo project list
+ *
+ * @author: yunzhi li
+ * @version: 2016/10/8 15:16
+ *           $Id$
+ */
+
+export default {
+    'pro': [
+        {
+            sn: '2016-SN12',
+            name: 'gqkuandai',
+            title: '陕西宽带国庆活动',
+            type: 'web',
+            dev: 'pc',
+            date: '2016-10-09',
+            remUnit: 20,
+            svn: ''
+        }, {
+            sn: '2016-SN03',
+            name: 'sxwt',
+            title: '陕西网厅客户关怀平台',
+            type: 'web',
+            dev: 'pc',
+            date: '2016-09-18',
+            remUnit: 20,
+            svn: ''
+        }, {
+            sn: '2017-CQ001',
+            name: 'tanchiji',
+            title: '重庆移动-微信小游戏-贪吃鸡',
+            type: 'wap',
+            dev: 'phone',
+            date: '2017-1-12',
+            remUnit: 75,
+            // less\css\sass\postcss
+            // 当前仍只支持前两种
+            compileCss: 'less',
+            svn: ''
+        }
+    ]
+}
+
+
