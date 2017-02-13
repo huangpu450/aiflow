5fe24f2dc5861b1bbb2f88657612877507b9cc51
lfire <yunzhi.li.08@gmail.com>
2017-02-13 09:38:11 +0800

    添加示例，说明静态文件分模块的使用，以及相关变量的使用方法，另外包括：循环、模板变量等语法

---
+++ archive/2016-SN03-sxwt-陕西网厅客户关怀平台/src/sxwt/config/data.js
@@ -0,0 +1,101 @@
+'use strict';
+/**
+ * the project data
+ *
+ * @author: yunzhi li
+ * @version: 2016/11/2 9:07
+ *           $Id$
+ */
+
+export default {
+    'pages': {
+        index: {
+            action: 'index',
+            title: '陕西网厅首页',
+            data: {
+                'curr': 'index',
+                'userInfo': {
+                    name: '星空',
+                    avatar: '/images/avatar_1.png',
+                    level: 6,
+                    levelName: '高手',
+                    levelImg: '/images/level_06.png',
+                    eNum: 488,
+                    curIntegral: 3001,
+                    nextLevelIntegral: 4000
+                },
+                'duihuanList': [
+                    {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        validDate: '2016-09-19至2016-09-30',
+                        zhekou: '9.5折',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '100元话费',
+                        btText: '10E币兑换',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '9.8折',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '100全国通用流量',
+                        btText: '50E币兑换',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '9.5折',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '9.8折',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '9.7折',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }, {
+                        img: '/images/i_p_001.png',
+                        title: '10元话费',
+                        btText: '10E币兑换',
+                        zhekou: '9.8',
+                        validDate: '2016-09-19至2016-09-30',
+                        count: '4582'
+                    }
+                ]
+            }
+        },
+        fuli: {
+            action: 'fuli',
+            title: '陕西网厅E币福利',
+            data: {
+                'curr': 'fuli'
+            }
+        }
+    }
+}
