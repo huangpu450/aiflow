0ca274129d65a6521de1a16852037f5239de922c
lfire <yunzhi.li.08@gmail.com>
2017-03-20 17:16:16 +0800

    相关文件格式化

--- src/common/config/initpro.js
+++ src/common/config/initpro.js
@@ -8,51 +8,47 @@
  */
 
 export default {
-    'pro': [
-        {
-            "sn": "2016-PI001",
-            "name": "init",
-            "title": "框架初始化样例项目",
-            "dev": "pc",
-            "author": "liyz",
-            "type": "web",
-            "date": "2016-11-21",
-            "remUnit": 20,
-            "compileCss": "css",
-            "minImg": false,
-            "minLevel": 3,
-            "svn": "",
-            "remark": ""
-        }, {
-            "sn": "2016-SN03",
-            "name": "sxwt",
-            "title": "陕西网厅客户关怀平台",
-            "dev": "pc",
-            "author": "liyz",
-            "type": "web",
-            "date": "2016-09-18",
-            "remUnit": 20,
-            "compileCss": "css",
-            "minImg": false,
-            "minLevel": 3,
-            "svn": "",
-            "remark": ""
-        }, {
-            "sn": "2016-HN026",
-            "name": "guodanian",
-            "title": "湖南移动-过大年聚合页",
-            "dev": "pc",
-            "author": "liyz,susj",
-            "type": "web",
-            "date": "2016-11-30",
-            "remUnit": 75,
-            "compileCss": "less",
-            "minImg": true,
-            "minLevel": 5,
-            "svn": "",
-            "remark": ""
-        }
-    ]
+    "pro": [{
+        "sn": "2016-PI001",
+        "name": "init",
+        "title": "框架初始化样例项目",
+        "dev": "pc",
+        "author": "liyz",
+        "type": "web",
+        "date": "2016-11-21",
+        "remUnit": 20,
+        "compileCss": "css",
+        "minImg": false,
+        "minLevel": 3,
+        "svn": "",
+        "remark": ""
+    }, {
+        "sn": "2016-SN03",
+        "name": "sxwt",
+        "title": "陕西网厅客户关怀平台",
+        "dev": "pc",
+        "author": "",
+        "type": "web",
+        "date": "2016-09-18",
+        "remUnit": 20,
+        "compileCss": "css",
+        "minImg": false,
+        "minLevel": 3,
+        "svn": "",
+        "remark": ""
+    }, {
+        "sn": "2016-HN026",
+        "name": "guodanian",
+        "title": "湖南移动-过大年聚合页",
+        "dev": "pc",
+        "author": "liyz,susj",
+        "type": "web",
+        "date": "2016-11-30",
+        "remUnit": 75,
+        "compileCss": "less",
+        "minImg": false,
+        "minLevel": 5,
+        "svn": "",
+        "remark": ""
+    }]
 }
-
-
